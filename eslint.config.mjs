70f267988b44f0a670a76422f415e3511e3ae3c6
muhammadrukon <muhammad.rukon242@gmail.com>
2025-09-25 02:55:55 +0600

    fix(lint): global ignore added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,18 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
+import { globalIgnores } from 'eslint/config';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  globalIgnores(['src/generated']), // ignores this directory
   {
-    ignorePatterns: ['src/generated/**/*'], // ignores all files recursively in the generated folder
     rules: {
       'react/no-display-name': 'off',
     },
