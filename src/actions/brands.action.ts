306ef44527029279f896aa32048fe2edef304a45
muhammadrukon <muhammad.rukon242@gmail.com>
2025-09-27 13:35:52 +0600

    chore: readme update

--- src/actions/brands.action.ts
+++ src/actions/brands.action.ts
@@ -2,31 +2,36 @@ import type { Brand } from '@prisma/client';
 import { IBrandCreateEntity } from '@/interfaces';
 
 export const BrandsAction = {
-  baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
+  api(id?: string) {
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL + '/api/brands';
+    if (id) return `${baseUrl}/${id}`;
+    return baseUrl;
+  },
+
   async create(brand: IBrandCreateEntity): Promise<Response> {
-    return await fetch(`${this.baseUrl}/api/brands`, {
+    return await fetch(this.api(), {
       method: 'POST',
       body: JSON.stringify(brand),
     });
   },
 
   async getAll(): Promise<Response> {
-    return await fetch(`${this.baseUrl}/api/brands`);
+    return await fetch(this.api());
   },
   //TODO:get brand by id
   async getById(id: string): Promise<Response> {
-    return await fetch(`${this.baseUrl}/api/brands/${id}`);
+    return await fetch(this.api(id));
   },
   //TODO:update brand
-  async update(id: string, payload: Brand) {
-    return await fetch(`${this.baseUrl}/api/brands/${id}`, {
+  async update(id: string, payload: Brand): Promise<Response> {
+    return await fetch(this.api(id), {
       method: 'PUT',
       body: JSON.stringify(payload),
     });
   },
   //TODO:delete brand (soft delete/status = archive)
   async remove(id: string): Promise<Response> {
-    return await fetch(`${this.baseUrl}/api/brands/${id}`, {
+    return await fetch(this.api(id), {
       method: 'DELETE',
     });
   },
